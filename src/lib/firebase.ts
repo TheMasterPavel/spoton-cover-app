074cbdb2149f00dfb1e2a50364cdc84c67e2d36a
Pavel Makarov <XDpavel@gmail.com>
2025-08-27 21:09:25 +0000

    Configurar Firebase y flujo de subida de imágenes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,4 @@
-
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// IMPORTANT: This is your actual Firebase config object.
-const firebaseConfig = {
+{
   "projectId": "spoton-cover",
   "appId": "1:727089932575:web:b17a0d5bbbae9e8aa8282a",
   "storageBucket": "spoton-cover.firebasestorage.app",
@@ -15,10 +6,4 @@ const firebaseConfig = {
   "authDomain": "spoton-cover.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "727089932575"
-};
-
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const storage = getStorage(app);
-
-export { app, storage };
+}
